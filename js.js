5d25f9c4533c9ac8898bb4e088f6842646900a85
Radwan <tara02664@gmail.com>
2022-10-26 00:01:29 +0200

    simple hour

---
+++ js.js
@@ -0,0 +1,46 @@
+setInterval(() => {
+	let hours = document.getElementById("hours");
+	let minutes = document.getElementById("minutes");
+	let seconds = document.getElementById("seconds");
+	let ampm = document.getElementById("ampm");
+
+	let hr_dot = document.querySelector(".hr_dot");
+	let min_dot = document.querySelector(".min_dot");
+	let sec_dot = document.querySelector(".sec_dot");
+
+	let hh = document.getElementById("hh");
+	let mm = document.getElementById("mm");
+	let ss = document.getElementById("ss");
+
+	let h = new Date().getHours();
+	let m = new Date().getMinutes();
+	let s = new Date().getSeconds();
+	let am = h >= 12 ? "PM" : "AM";
+
+	// convert 24hr clock to 12hr clock
+	if (h > 12) {
+		h = h - 12;
+	}
+	// add 0 before single digit number
+
+	h = h < 10 ? "0" + h : h;
+	s = s < 10 ? "0" + s : s;
+	m = m < 10 ? "0" + m : m;
+
+	hh.style.strokeDashoffset = 440 - (440 * h) / 12;
+	mm.style.strokeDashoffset = 440 - (440 * m) / 60;
+	ss.style.strokeDashoffset = 440 - (440 * s) / 60;
+
+	hr_dot.style.transform = `rotate(${h * 30}deg)`;
+	min_dot.style.transform = `rotate(${m * 6}deg)`;
+	sec_dot.style.transform = `rotate(${s * 6}deg)`;
+
+	hours.innerHTML = h + "<br><span>Hours</span>";
+	minutes.innerHTML = m + "<br><span>Minutes</span>";
+	seconds.innerHTML = s + "<br><span>Seconds</span>";
+	ampm.innerHTML = am;
+
+	hh.style.strokeDashoffset = 440 - (440 * h) / 12;
+	mm.style.strokeDashoffset = 440 - (440 * m) / 60;
+	ss.style.strokeDashoffset = 440 - (440 * s) / 60;
+});
