a4891ae040f945e64514872cf89f281fbd31e41d
Ahmed Tarek Radwan <100035760+ahmedradwan21@users.noreply.github.com>
2022-10-26 00:13:20 +0200

    Update js.js

--- js.js
+++ js.js
@@ -47,7 +47,14 @@ setInterval(() => {
 hh.style.strokeDashoffset = 440 - (440 * h) / 12;
 	mm.style.strokeDashoffset = 440 - (440 * m) / 60;
 	ss.style.strokeDashoffset = 440 - (440 * s) / 60;
-// 
+hh.style.strokeDashoffset = 440 - (440 * h) / 12;
+	mm.style.strokeDashoffset = 440 - (440 * m) / 60;
+	ss.style.strokeDashoffset = 440 - (440 * s) / 60;hh.style.strokeDashoffset = 440 - (440 * h) / 12;
+	mm.style.strokeDashoffset = 440 - (440 * m) / 60;
+	ss.style.strokeDashoffset = 440 - (440 * s) / 60;hh.style.strokeDashoffset = 440 - (440 * h) / 12;
+	mm.style.strokeDashoffset = 440 - (440 * m) / 60;
+	ss.style.strokeDashoffset = 440 - (440 * s) / 60; 
+
 // 
 // 
 // 
